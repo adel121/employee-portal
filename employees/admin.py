01a46ad3f4ff3e04bbda7e09d67414061bbf6c62
Adel Haj Hassan <adel_hajhasan@hotmail.com>
2023-08-19 21:20:09 +0200

    add employee label to admin dashboard

--- employees/admin.py
+++ employees/admin.py
@@ -5,10 +5,11 @@ from .models import User
 admin.site.register(User, UserAdmin)
 # Register your models here.
 
-from .models import Employee, Site, WorkSlot, Administrator, Manager
+from .models import Employee, Site, WorkSlot, Administrator, Manager, EmployeeLabel
 
 admin.site.register(Employee)
 admin.site.register(Site)
 admin.site.register(WorkSlot)
 admin.site.register(Administrator)
 admin.site.register(Manager)
+admin.site.register(EmployeeLabel)
